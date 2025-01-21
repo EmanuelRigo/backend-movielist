aa820f72925bf762d4c77c470ac1778ee3be2357
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-01-20 21:24:42 -0300

    creating services and controllers

---
+++ src/routes/api/index.api.js
@@ -0,0 +1,18 @@
+import CustomRouter from "../../utils/customRouter.util";
+
+class ApiRouter extends CustomRouter {
+  constructor() {
+    super();
+    this.init()
+  }
+
+    init = () => {
+        this.use("/movies", ["PUBLIC"], moviesApiRouter);
+        this.use("/users", ["PUBLIC"], usersApiRouter);
+        this.use("/sessions", ["PUBLIC"], sessionsApiRouter);
+        this.use("/cookies", ["PUBLIC"], cookiesApiRouter);
+    }
+}
+
+const apiRouter = new ApiRouter();
+export default apiRouter;
